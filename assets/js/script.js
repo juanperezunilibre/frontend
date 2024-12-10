dbb220f1acd0d78097dadbf9b5b7f7195e80c33d
Juan <juan.perez@unilibre.edu.co>
2024-12-09 22:02:57 -0500

    listar clientes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,67 @@
-document.addEventListener('DOMContentLoaded', function() {
+import { api } from "./utils.js";
 
-    const form = document.querySelector('form');
+document.addEventListener("DOMContentLoaded", function () {
+  cargarDatosCliente(); // llamado a la funcion cargarDatosCliente
 
-    const { documento, nombre, apellidos, email } = form.elements; // Destructuring: recupera los elementos del formulario
+  const form = document.querySelector("form");
 
-    form.addEventListener("submit", function(event) {
-        event.preventDefault(); // Evita que se recargue la página
+  const { documento, nombre, apellidos, email } = form.elements; // Destructuring: recupera los elementos del formulario
 
-        const data = {
-            documento: documento.value,
-            nombre: nombre.value,
-            apellidos: apellidos.value,
-            email: email.value
-        }
+  form.addEventListener("submit", function (event) {
+    event.preventDefault(); // Evita que se recargue la página
 
-        // enviar los datos
-        fetch("http://localhost:3000/cliente", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data)
-        })
-        .then(res => res.json())
-        .then(data => {
-            alert(data.message);
-            form.reset();
-        })
-        .catch(err => console.error(err))
+    const data = {
+      documento: documento.value,
+      nombre: nombre.value,
+      apellidos: apellidos.value,
+      email: email.value,
+    };
 
-    })
-})
+    // enviar los datos
+    api
+      .post("/cliente", data)
+      .then(({ data }) => {
+        console.log(data);
+        alert(data.message);
+        cargarDatosCliente();
+        form.reset();
+      })
+      .catch((err) => alert("Error!: " + err?.response?.data?.message));
+  });
+});
+
+function editarCliente() {
+    alert("Editar cliente");
+}
+
+function cargarDatosCliente() {
+  const tbody = document.querySelector("tbody");
+  tbody.innerHTML = "";
+  // peticion a localhost:3000/clientes del server de node
+  api.get("/clientes").then(({ data }) => {
+    
+    for (const cliente of data) {
+      tbody.innerHTML += `
+          <tr>
+            <td>${cliente.id}</td>
+            <td>${cliente.documento}</td>
+            <td>${cliente.nombre}</td>
+            <td>${cliente.apellidos}</td>
+            <td>${cliente.email}</td>
+            <td>${cliente.fecha_nacimiento}</td>
+            <td>
+                <div class="btn-group" role="group" aria-label="actions">
+                    <button type="button" class="btn btn-primary">
+                       <i class="fa-solid fa-pen-to-square"></i>
+                    </button>
+                    <button type="button" class="btn btn-danger">
+                        <i class="fa-solid fa-trash-can"></i>
+                    </button>
+                </div>
+            </td>
+          </tr>`;
+    }
+  });
+}
+
+
